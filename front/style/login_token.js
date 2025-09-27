e0d6ad41f516bba0d7c1cab7b29de2dc0d7662c5
balintmolnar21 <molnar.balint.otto@gmail.com>
2025-09-27 11:27:01 +0200

    login_mod

--- front/style/login_token.js
+++ front/style/login_token.js
@@ -34,7 +34,7 @@ window.addEventListener("DOMContentLoaded", () => {
       localStorage.setItem('user', JSON.stringify(data.user));
 
       // 🔹 Átirányítás az index.html-re
-      window.location.href = '/index.html';
+      window.location.href = '/index';
     } catch (err) {
       console.error(err);
       errorMessage.innerText = 'Szerver hiba';
